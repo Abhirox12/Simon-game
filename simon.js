32b27c71a9fd2c443cef78d9b9af4409b9c8183a
Abhimanyu Sharma <Abhiroxatwork@gmail.com>
2025-12-01 23:53:53 +0530

    added dialog boxes of winning and loses to make game more authentic

--- simon.js
+++ simon.js
@@ -3,6 +3,8 @@ let red = document.querySelector(".red")
 let green = document.querySelector(".green")
 let blue = document.querySelector(".blue")
 let yellow = document.querySelector(".yellow")
+let win = document.querySelector(".win")
+let lose = document.querySelector(".lose")
 let body = document.querySelector("body")
 let result = document.querySelector(".result")
 let boxes = document.querySelectorAll(".box")
@@ -80,25 +82,34 @@ function inp() {
     let i = storeArr.length - 1;
     let total = score;
     if (storeArr[i] !== arr[i]) {
-        body.classList.remove("LosingAnimation")
-        void this.offsetWidth;
-        body.classList.add("LosingAnimation")
-        // resetGame();
-        result.innerText = `Wrong! Game Over. Your Score is ${total}`;
-        gameover()
-        arr = [];
+        if (storeArr.length > arr.length) {
+            result.innerText = `please start the game first`;
+        }
+        else {
 
-        return;
+            body.classList.remove("LosingAnimation")
+            void this.offsetWidth;
+            body.classList.add("LosingAnimation")
+            // resetGame();
+            lose.style.display = "flex";
+            result.innerText = `Wrong! Game Over. Your Score is ${total}`;
+
+            gameover()
+            arr = [];
+
+            return;
+        }
     }
 
     if (storeArr.length === arr.length) {
         result.innerText = `congratulations you won level ${level}`
+        win.style.display = "flex";
         level += 1;
         score += 1;
         h3.innerText = `Level ${level}`;
         Winning()
 
-        setTimeout(colorGen, 2000); // move to next level
+        // setTimeout(colorGen, 4000); // move to next level
     }
 
 }
@@ -107,16 +118,17 @@ function inputclicksound() {
 }
 
 
-function gameover (){
+
+function gameover() {
     setTimeout(() => {
         gameoversound.play().catch(err => console.warn('Audio play failed:', err));
-  }, 1000);
+    }, 1000);
 }
 
-function Winning (){
-      setTimeout(() => {
-    Winningsound.play().catch(err => console.warn('Audio play failed:', err));
-     }, 1000);
+function Winning() {
+    setTimeout(() => {
+        Winningsound.play().catch(err => console.warn('Audio play failed:', err));
+    }, 1000);
 }
 btn.addEventListener("click", function () {
     arr = [];
@@ -126,3 +138,38 @@ btn.addEventListener("click", function () {
     result.innerText = "";
     colorGen();
 })
+function restart() {
+    level = 1;
+    score = 0;
+    h3.innerText = `Level ${level}`;
+    result.innerText = "";
+    // score = 0;
+    setTimeout(() => {
+        colorGen()
+    }, 1000);
+    //   arr = [];
+
+    lose.style.display = "none";
+}
+function nextlevel() {
+    setTimeout(() => {
+        colorGen()
+    }, 1000);
+
+
+    win.style.display = "none";
+}
+function reset() {
+    win.style.display="none";
+     level = 1;
+    score = 0;
+    h3.innerText = `Level ${level}`;
+    result.innerText = "";
+}
+function exit() {
+    lose.style.display="none";
+     level = 1;
+    score = 0;
+    h3.innerText = `Level ${level}`;
+    result.innerText = "";
+}
